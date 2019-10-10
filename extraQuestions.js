30d3f77c88c3e1bbfb839a20dd83627ad146c2a5
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-09 20:14:14 -0400

    complete 1 & 2

---
+++ extraQuestions.js
@@ -0,0 +1,28 @@
+// let cat = {
+//     genus: 'Felis',
+//     species: 'Catus'
+//  }
+
+//  console.log(cat["species"]);
+//  cat["color"] = "grey";
+
+//  if(cat.hasOwnProperty("texture")) {
+//      console.log(`Cat has texture`);
+//  } else {
+//      console.log(`Cat does not have texture`);
+//  }
+
+
+//  let p1 = {
+//     name: 'Joe'
+//   }
+
+//   let p2 = {
+//     name: 'Joe'
+//   }
+//   console.log(p1 === p2)
+// It would log false, because the memory points to different points.
+
+let spanishNumbers = {1: "uno", 2: "dos", 3: "tres", 4: "quatro", 5: "cinco"}
+let arrSpanNum = Object.keys(spanishNumbers);
+console.log(arrSpanNum);
