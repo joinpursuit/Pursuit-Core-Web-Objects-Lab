8750db10fcd3ced81aa73cbe120e9286a703dbba
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-09 20:18:41 -0400

    complete problem 4 - extra questions

--- extraQuestions.js
+++ extraQuestions.js
@@ -1,3 +1,4 @@
+//PROBLEM 1
 // let cat = {
 //     genus: 'Felis',
 //     species: 'Catus'
@@ -12,7 +13,7 @@
 //      console.log(`Cat does not have texture`);
 //  }
 
-
+//PROBLEM 2
 //  let p1 = {
 //     name: 'Joe'
 //   }
@@ -23,8 +24,23 @@
 //   console.log(p1 === p2)
 // It would log false, because the memory points to different points.
 
-let spanishNumbers = {1: "uno", 2: "dos", 3: "tres", 4: "quatro", 5: "cinco"}
-let arrSpanNumKey = Object.keys(spanishNumbers);
-console.log(arrSpanNumKey);
-let arrSpanNumVal = Object.values(spanishNumbers);
-console.log(arrSpanNumVal);
+
+//PROBLEM 3
+// let spanishNumbers = {1: "uno", 2: "dos", 3: "tres", 4: "quatro", 5: "cinco"}
+// let arrSpanNumKey = Object.keys(spanishNumbers);
+// console.log(arrSpanNumKey);
+// let arrSpanNumVal = Object.values(spanishNumbers);
+// console.log(arrSpanNumVal);
+
+
+//PROBLEM 4
+let obj = {
+    1: 1,
+    2: 2,
+    3: 3
+};
+let numOfProperties = 0;
+for(let key in obj) {
+    numOfProperties += 1;
+}
+console.log(numOfProperties);
