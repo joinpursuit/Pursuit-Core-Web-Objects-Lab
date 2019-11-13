afb1bcf277cb0659056efd19ab686311fcda0cb6
serami12 <stephanieramirez1@pursuit.org>
2019-11-13 18:47:30 -0500

    answered some of the extra lab questions

--- lab.js
+++ lab.js
@@ -12,7 +12,7 @@ var applesObj = {
 
 // a. Set eveAppleCount equal to the number of apples that Eve has
 let eveAppleCount = applesObj.Eve
-// assert(eveAppleCount === 4)
+assert(eveAppleCount === 4)
 
 // b. Change the number of apples that Adam  has to 4
 
@@ -24,7 +24,7 @@ console.log(applesObj.Adam)
 
 let calAndDanAppleCount = applesObj.Cal + applesObj.Dan
 console.log(calAndDanAppleCount)
-// assert(calAndDanAppleCount === 8)
+assert(calAndDanAppleCount === 8)
 
 // d. Set all the values in applesObj to 0
 for (let key in applesObj) {
@@ -32,144 +32,142 @@ for (let key in applesObj) {
     // console.log(applesObj[key])
 }
 // console.log(applesObj)
-// for (let key in applesObj) {
-//     assert(applesObj[key] === 0)
-// }
+for (let key in applesObj) {
+    // assert(applesObj[key] === 0)
+    // }
 
 
-// Question Two
-var citiesObj = {
-    Afghanistan: "Kabul",
-    Russia: "Moscow",
-    Iceland: "Reykjavik"
-}
-
-// a. Set russiaCapital equal to Russia's capital using citiesObj
-let russiaCapital = citiesObj["Russia"]
-// assert(russiaCapital === "Moscow")
-
-// // b. Add a new key value pair "Jamaica" and its capital "Kingston"
-// citiesObj["Jamaica"] = "Kingston";
-// console.log(citiesObj)
-// assert(citiesObj["Jamaica"] === "Kingston")
-
-// // c. Add a new key value pair "Indonesia" and its capital "Jakarta"
-// // Your code here
-// citiesObj["Indonesia"] = "Jakarta";
-// console.log(citiesObj)
-// assert(citiesObj["Indonesia"] === "Jakarta")
-
-// Question 3
-
-// a. Create a object that represents the table below listing an authors name and their comprehensibility score.
-/*
-| Author Name |    Score |
-| :--: | :--: |
-| Mark Twain |    8.9 |
-| Nathaniel Hawthorne    | 5.1 |
-| John Steinbeck    | 2.3 |
-| C.S. Lewis | 9.9 |
-| Jon Krakauer | 6.1 |
-*/
-
-// var authorScores = {
-//     "Mark Twain": 8.9,
-//     "Nathaniel Hawthorne": 5.1,
-//     "John Steinbeck": 2.3,
-//     "C.S. Lewis": 9.9,
-//     "Jon Krakauer": 6.1
-// }
-// console.log(authorScores);
-// assert(authorScores["Mark Twain"] === 8.9)
-// assert(authorScores["Nathaniel Hawthorne"] === 5.1)
-// assert(authorScores["John Steinbeck"] === 2.3)
-// assert(authorScores["C.S. Lewis"] === 9.9)
-// assert(authorScores["Jon Krakauer"] === 6.1)
-
-
-// b. Add an additional author named “Erik Larson” with an assigned score of 9.2.
-// Your code here
-// authorScores["Erik Larson"] = "score:9.2"
-// assert(authorScores["Erik Larson"] === 9.2)
-
-// Question Four
-// You are given an array of objects. Each object in the array describes the score of a person. Find the person with the best score and print his full name.
-
-var peopleWithScores = [{
-        firstName: "Calvin",
-        lastName: "Newton",
-        score: 13
-    },
-    {
-        firstName: "Garry",
-        lastName: "Mckenzie",
-        score: 23
-    },
-    {
-        firstName: "Leah",
-        lastName: "Rivera",
-        score: 10
-    },
-    {
-        firstName: "Sonja",
-        lastName: "Moreno",
-        score: 3
-    },
-    {
-        firstName: "Noel",
-        lastName: "Bowen",
-        score: 16
+    // Question Two
+    var citiesObj = {
+        Afghanistan: "Kabul",
+        Russia: "Moscow",
+        Iceland: "Reykjavik"
     }
-]
 
-var highestScoringName = ""
+    // a. Set russiaCapital equal to Russia's capital using citiesObj
+    let russiaCapital = citiesObj["Russia"]
+    // assert(russiaCapital === "Moscow")
 
-// Your code here
+    // // b. Add a new key value pair "Jamaica" and its capital "Kingston"
+    citiesObj["Jamaica"] = "Kingston";
+    console.log(citiesObj)
+    // assert(citiesObj["Jamaica"] === "Kingston")
 
-// assert(highestScoringName === "Garry Mckenzie")
+    // // c. Add a new key value pair "Indonesia" and its capital "Jakarta"
+    // // Your code here
+    citiesObj["Indonesia"] = "Jakarta";
+    console.log(citiesObj)
+    // assert(citiesObj["Indonesia"] === "Jakarta")
 
-// Question Five
+    // Question 3
 
-// Write code below such that cubeObj maps the numbers 
-//between 1 and 20 inclusive to their cubes.
-//A number 's cube is that number multiplied by itself twice:
-// 2 ^ 3 = 2 * 2 * 2 = 8
+    // a. Create a object that represents the table below listing an authors name and their comprehensibility score.
+    /*
+    | Author Name |    Score |
+    | :--: | :--: |
+    | Mark Twain |    8.9 |
+    | Nathaniel Hawthorne    | 5.1 |
+    | John Steinbeck    | 2.3 |
+    | C.S. Lewis | 9.9 |
+    | Jon Krakauer | 6.1 |
+    */
 
-var cubeObj = {}
-for (let i = 1; i < 20; i++) {
-    cubeObj[i] = Math.pow(i, 3)
-}
-// console.log()
-
-
-// assert(Object.keys(cubeObj).length === 20)
-// assert(cubeObj[1] === 1)
-// assert(cubeObj[2] === 8)
-// assert(cubeObj[3] === 27)
-// assert(cubeObj[14] === 2744)
-// assert(cubeObj[20] === 8000)
-
-// Question Six
-
-// Find the most common letter in the string below.  
-//Use an object to generate your solution that maps a character to
-//the number of times it appears in the string.Ignore whitespaces and capitalization.
-var myString = "We're flooding people with information. We need to feed it through a processor. A human must turn information into intelligence or knowledge. We've tended to forget that no computer will ever ask a new question."
-
-var frequencyObj = {}
-
-for (let i = 0; i < myString.length; i++) {
-    if (frequencyObj[myString[i]] === undefined) {
-        frequencyObj[myString[i]] = 1
-    } else {
-        frequencyObj[myString[i]] += 1
+    var authorScores = {
+        "Mark Twain": 8.9,
+        "Nathaniel Hawthorne": 5.1,
+        "John Steinbeck": 2.3,
+        "C.S. Lewis": 9.9,
+        "Jon Krakauer": 6.1
     }
-}
-console.log(frequencyObj)
-// var mostFrequentChar
+    console.log(authorScores);
+    assert(authorScores["Mark Twain"] === 8.9)
+    assert(authorScores["Nathaniel Hawthorne"] === 5.1)
+    assert(authorScores["John Steinbeck"] === 2.3)
+    assert(authorScores["C.S. Lewis"] === 9.9)
+    assert(authorScores["Jon Krakauer"] === 6.1)
 
-// Your code here
 
-//assert(mostFrequentChar === "e")
+    // b. Add an additional author named “Erik Larson” with an assigned score of 9.2.
+    // Your code here
+    authorScores["Erik Larson"] = "score:9.2"
+    assert(authorScores["Erik Larson"] === 9.2)
 
-console.log("Ran with 0 errors")
+    // Question Four
+    // You are given an array of objects. Each object in the array describes the score of a person. Find the person with the best score and print his full name.
+
+    var peopleWithScores = [{
+            firstName: "Calvin",
+            lastName: "Newton",
+            score: 13
+        },
+        {
+            firstName: "Garry",
+            lastName: "Mckenzie",
+            score: 23
+        },
+        {
+            firstName: "Leah",
+            lastName: "Rivera",
+            score: 10
+        },
+        {
+            firstName: "Sonja",
+            lastName: "Moreno",
+            score: 3
+        },
+        {
+            firstName: "Noel",
+            lastName: "Bowen",
+            score: 16
+        }
+    ]
+
+    var highestScoringName = ""
+
+    // Your code here
+
+    assert(highestScoringName === "Garry Mckenzie")
+
+    // Question Five
+
+    // Write code below such that cubeObj maps the numbers 
+    //between 1 and 20 inclusive to their cubes.
+    //A number 's cube is that number multiplied by itself twice:
+    // 2 ^ 3 = 2 * 2 * 2 = 8
+
+    var cubeObj = {}
+    for (let i = 1; i < 20; i++) {
+        cubeObj[i] = Math.pow(i, 3)
+    }
+    // console.log()
+
+
+    assert(Object.keys(cubeObj).length === 20)
+    assert(cubeObj[1] === 1)
+    assert(cubeObj[2] === 8)
+    assert(cubeObj[3] === 27)
+    assert(cubeObj[14] === 2744)
+    assert(cubeObj[20] === 8000)
+
+    // Question Six
+
+    // Find the most common letter in the string below.  
+    //Use an object to generate your solution that maps a character to
+    //the number of times it appears in the string.Ignore whitespaces and capitalization.
+    var myString = "We're flooding people with information. We need to feed it through a processor. A human must turn information into intelligence or knowledge. We've tended to forget that no computer will ever ask a new question."
+
+    var frequencyObj = {}
+
+    for (let i = 0; i < myString.length; i++) {
+        if (frequencyObj[myString[i]] === undefined) {
+            frequencyObj[myString[i]] = 1
+        } else {
+            frequencyObj[myString[i]] += 1
+        }
+    }
+    // console.log(frequencyObj)
+    // var mostFrequentChar
+
+    // assert(mostFrequentChar === "e")
+
+    // console.log("Ran with 0 errors")
