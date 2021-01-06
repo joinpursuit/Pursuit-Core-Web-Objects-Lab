cabf5b264e2ef6fa505660e529d347fcc4d57272
Coreen Cooper <coreencooper@pursuit.org>
2021-01-05 19:58:11 -0500

    solve eveAppleSet()

--- tests/objets.test.js
+++ tests/objets.test.js
@@ -86,7 +86,7 @@ describe("appleCount", () => {
   });
 });
 
-describe("eveAppleSet", () => {
+describe.only("eveAppleSet", () => {
   test("updates Eve's apple count", () => {
     expect(
       eveAppleSet(
